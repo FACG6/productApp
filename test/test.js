ec305b47ed5c6877bbed1be387f2a3d4ca03dcee
shorouq <shrooqabdullahsaad@gmail.com>
2019-02-12 15:21:31 +0200

    modify error from eslint

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const test = require('tape');
-test('test', (test) => {
-    test.equal(1, 1, "not equal");
-    test.end();
-})
+
+test('test', (t) => {
+  t.equal(1, 1, 'not equal');
+  t.end();
+});
