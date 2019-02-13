60164707f3394df027630bd888d73471a5b4e9dd
shorouq <shrooqabdullahsaad@gmail.com>
2019-02-13 04:11:02 +0200

    making the the xhr from front

---
+++ public/js/request.js
@@ -0,0 +1,19 @@
+const request = (url, method, value, cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      const response = JSON.parse(xhr.responseText);
+      if (xhr.status === 200) {
+        if (response.error) {
+          cb(new TypeError(response.error));
+        } else {
+          cb(null, JSON.parse(response.results));
+        }
+      } else {
+        cb(new TypeError(response.error));
+      }
+    }
+  };
+  xhr.open(method, url);
+  xhr.send(value);
+};
