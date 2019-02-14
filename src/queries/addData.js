851f4d7342a9fed94257543608e9add217cb3146
shorouq <shrooqabdullahsaad@gmail.com>
2019-02-14 09:45:48 +0200

    edit insert query

--- src/queries/addData.js
+++ src/queries/addData.js
@@ -2,9 +2,9 @@ const dbConnection = require('../database/db_connection.js');
 
 const addProduct = (name, proDate, expDate, companyId, cb) => {
   dbConnection.query(
-    `INSERT INTO product (name, pro_date, exp_date, company_id) VALUES ('${name}', '${proDate}', '${expDate}', ${companyId})`, (err, res) => {
+    'INSERT INTO product (name, pro_date, exp_date, company_id) VALUES ($1, $2, $3, $4) RETURNING *', [name, proDate, expDate, companyId], (err, res) => {
       if (err) return cb(err);
-      return cb(null, res);
+      return cb(null, res.rows);
     },
   );
 };
